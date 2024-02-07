441ebc5ad77f26d5e07c3bf93596d9baf04321cf
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2024-02-07 21:40:48 +0900

    회원가입 기능 구현
    프론트엔드 및 백엔드 작업 (signup)

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import sqlite3
 con = sqlite3.connect('db.db', check_same_thread=False)
 cur = con.cursor()
 
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items (
+	        id INTEGER PRIMARY KEY,
+	        title TEXT NOT NULL,
+        	image BLOB,
+	        price INTEGER NOT NULL,
+	        description TEXT ,
+        	place TEXT NOT NULL,
+	        insertAt INTEGER NOT NULL
+            );
+            """)
+
 app = FastAPI()
 
 #글쓰기 내용 저장
@@ -45,6 +57,19 @@ async def get_image(item_id):
                               SELECT image from items WHERE id={item_id}
                               """).fetchone()[0]
     return Response(content=bytes.fromhex(image_bytes))
+
+@app.post('/signup')
+def signup(id:Annotated[str,Form()], 
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+    cur.execute(f"""
+                INSERT INTO users(id, name, email, password)
+                VALUES ('{id}','{name}','{email}','{password}')
+                """)
+    print(password)
+    con.commit()
+    return '200'
 
 #하위 > 상위 경로로 순차적으로 작성해야 한다. 상위 경로가 먼저 작성되면 모든 요청을 상위 경로것으로 가져오게 된다.
 app.mount("/",StaticFiles(directory="frontend",html=True),name="frontend")
